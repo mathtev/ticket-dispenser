9959f53036a3a9474a409e1e73bdbd14059e84d7
mathtev <42418426+mathtev@users.noreply.github.com>
2020-05-25 14:29:48 +0200

    edit ticet labels

--- interf.py
+++ interf.py
@@ -2,6 +2,9 @@ import Automat as aut
 import tkinter as tk
 from tkinter import font  as tkfont
 
+#TODO
+#Drugie okno z monetami i wpisywaniem ich liczby
+#Wydawanie reszty z wrzuconych monet
 
 automat = aut.Automat(aut.obslugiwane_nominaly)
 
@@ -44,31 +47,41 @@ class MainWindow(tk.Frame):
         self.controller = controller
 
         self.main_font = tkfont.Font(family='Helvetica', size=18, weight="bold")
-        
-        label1 = tk.Label(self, width=27, text="20-minutowy\nNormalny\t\t2.40 zł", font=self.main_font, fg="white", bg="blue", borderwidth=4, relief="groove", padx=5, pady=10, justify=tk.LEFT, anchor="w")
-        label2 = tk.Label(self, width=27, text="40-minutowy\nNormalny\t\t3.70 zł", font=self.main_font, fg="white", bg="blue", borderwidth=4, relief="groove", padx=5, pady=10, justify=tk.LEFT, anchor="w")
-        label3 = tk.Label(self, width=27, text="60-minutowy\nNormalny\t\t4.20 zł", font=self.main_font, fg="white", bg="blue", borderwidth=4, relief="groove", padx=5, pady=10, justify=tk.LEFT, anchor="w")
-        label4 = tk.Label(self, width=27, text="20-minutowy\nUlgowy\t\t\t1.70 zł", font=self.main_font, fg="white", bg="blue", borderwidth=4, relief="groove", padx=5, pady=10, justify=tk.LEFT, anchor="w")
-        label5 = tk.Label(self, width=27, text="40-minutowy\nUlgowy\t\t\t2.20 zł", font=self.main_font, fg="white", bg="blue", borderwidth=4, relief="groove", padx=5, pady=10, justify=tk.LEFT, anchor="w")
-        label6 = tk.Label(self, width=27, text="60-minutowy\nUlgowy\t\t\t2.50 zł", font=self.main_font, fg="white", bg="blue", borderwidth=4, relief="groove", padx=5, pady=10, justify=tk.LEFT, anchor="w")
-        label1.grid(row=0, column=0)
-        label2.grid(row=1, column=0)
-        label3.grid(row=2, column=0)
-        label4.grid(row=3, column=0)
-        label5.grid(row=4, column=0)
-        label6.grid(row=5, column=0)
+        self.plus_minus_font = tkfont.Font(family='Helvetica', size=24, weight="bold")
 
-        lb = 6              #liczba rodzai biletów
-        wybrane = [0]*lb    #liczba wybranych biletów dla każdego typu biletu
+        liczba_biletow = 6      #liczba rodzai biletów
+
+        lb_bilety = []
+        nazwy_biletow = [   "20-minutowy\nNormalny\t\t2.40 zł",
+                            "40-minutowy\nNormalny\t\t3.70 zł",
+                            "60-minutowy\nNormalny\t\t4.20 zł",
+                            "20-minutowy\nUlgowy\t\t\t1.70 zł",
+                            "40-minutowy\nUlgowy\t\t\t2.20 zł",
+                            "60-minutowy\nUlgowy\t\t\t2.50 zł"]
+
+        wybrane = [0]*liczba_biletow    #liczba wybranych biletów dla każdego typu biletu
         lb_wybrane = []     #lista etykiet zawierających liczbę wybranych biletów
 
         lb_cena = tk.Label(self, text="Do zapłaty: %.2f" % automat.doZaplaty, font=self.main_font)
         lb_cena.grid(row = 7, column = 2, columnspan=3)
 
+        plus_btns = []
+        minus_btns = []
+        
+
+        for i in range(liczba_biletow):
+            lb_bilet = tk.Label(self, width=27, text=nazwy_biletow[i], font=self.main_font, fg="white", bg="blue", borderwidth=4, relief="groove", padx=5, pady=10, justify=tk.LEFT, anchor="w")
+            lb_bilet.grid(row=i, column=0)
+            
+            lb_bilety.append(lb_bilet)
+
+
         def update_price():
             lb_cena.config(text="Do zapłaty: %.2f" % automat.doZaplaty)
 
         def plus_pressed(idx):
+            if minus_btns[idx]["state"] == "disabled":
+                minus_btns[idx]["state"] = "normal"
             automat.doZaplaty += automat.ceny_biletow[idx]
             update_price()
             wybrane[idx] += 1
@@ -80,18 +93,23 @@ class MainWindow(tk.Frame):
                 update_price()
                 wybrane[idx] -= 1
                 lb_wybrane[idx].config(text="%i" % wybrane[idx])
+            if wybrane[idx] == 0:
+                minus_btns[idx]["state"] = "disabled"
 
-        for i in range(lb):
+        for i in range(liczba_biletow):
             liczba = tk.Label(self, text="%i" % wybrane[i], font=self.main_font)
             liczba.grid(row = i, column = 3)
             lb_wybrane.append(liczba)
 
-        for i in range(lb):
-            plus = tk.Button(self, text = '+', command=lambda idx=i: plus_pressed(idx), pady=10, padx=20, font=self.main_font)
-            minus = tk.Button(self, text = '-', command=lambda idx=i: minus_pressed(idx), pady=10, padx=20, font=self.main_font)
-
+        for i in range(liczba_biletow):
+            plus = tk.Button(self, text = '+', command=lambda idx=i: plus_pressed(idx), pady=10, padx=20, font=self.plus_minus_font)
+            minus = tk.Button(self, text = '-', command=lambda idx=i: minus_pressed(idx), pady=10, padx=20, font=self.plus_minus_font)
             plus.grid(row = i, column = 2,padx=30,sticky="we")
             minus.grid(row = i, column = 4,padx=30,sticky="we")
+            
+            plus_btns.append(plus)
+            minus_btns.append(minus)
+            minus["state"] = "disabled"
 
 
         button1 = tk.Button(self, text="Kupuję i płacę", command=lambda: controller.show_frame("PageOne"), width=20, font=self.main_font, bg="green", fg="white")
